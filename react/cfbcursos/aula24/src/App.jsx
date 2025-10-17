8f94aa1e2a7472dcd74172c10976d9c0f1d42b9c
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2025-10-16 23:17:25 -0300

    Estudos - React - CFB Cursos
    Continuação dos estudos de React com CFB Cursos.
    
    Aula 24 e 25.

---
+++ react/cfbcursos/aula24/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Carro from './assets/components/carro'
+
+function App() {
+
+  return (
+    <>
+      <Carro fator={1}/>
+    </>
+  )
+}
+
+export default App
