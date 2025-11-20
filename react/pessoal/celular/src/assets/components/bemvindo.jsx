bd0bd0e5c3e3771af8f64f5bee7de7bc32cbb564
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2025-11-19 22:52:07 -0300

    Estudos - React - Projetos - Celular
    Pagina Bemvindo Finalizada.
    
    Programação da pagina concluída com sucesso.

--- react/pessoal/celular/src/assets/components/bemvindo.jsx
+++ react/pessoal/celular/src/assets/components/bemvindo.jsx
@@ -3,8 +3,44 @@ import './css/bemvindo.css'
 import '../../App.css'
 
 import htmlIcon from '../img/html.png'
+import cssIcon from '../img/css.png'
+import jsIcon from '../img/js.png'
+import reactIcon from '../img/react.png'
 
 export default function Bemvindo(){
+    
+    const linguagens=[
+        {nome: "html", img: htmlIcon, texto: "Html"},
+        {nome: "css", img: cssIcon, texto: "Css"},
+        {nome: "javascript", img: jsIcon, texto: "JavaScript"},
+        {nome: "react", img: reactIcon, texto: "React"}
+    ]
+
+    const[linguagemAtual,setLinguagemAtual]=useState(linguagens[0])
+
+    const linguagemAvancar=()=>{
+        if(linguagemAtual.nome=="html"){
+            setLinguagemAtual(linguagens[1]);
+        }else if(linguagemAtual.nome=="css"){
+            setLinguagemAtual(linguagens[2]);
+        }else if(linguagemAtual.nome=="javascript"){
+            setLinguagemAtual(linguagens[3]);
+        }else{
+            setLinguagemAtual(linguagens[0]);
+        }
+    }
+
+    const linguagemRetornar=()=>{
+        if(linguagemAtual.nome=="html"){
+            setLinguagemAtual(linguagens[3]);
+        }else if(linguagemAtual.nome=="react"){
+            setLinguagemAtual(linguagens[2]);
+        }else if(linguagemAtual.nome=="javascript"){
+            setLinguagemAtual(linguagens[1]);
+        }else{
+            setLinguagemAtual(linguagens[0]);
+        }
+    }
 
     return(
         <div className="container">
@@ -18,18 +54,19 @@ export default function Bemvindo(){
                             <span className="welcome-lista-text-item">Tecnologias utilizadas</span>
                         </div>
                         <div className="welcome-lista-elemento">
-                            <button className="welcome-lista-elemento-retornar ri-arrow-left-s-line"></button>
+                            <button className="welcome-lista-elemento-button ri-arrow-left-s-line" onClick={linguagemRetornar}></button>
                             <div className="welcome-lista-elemento-linguagens">
-                                <img className="welcome-lista-elemento-linguagens-img" src={htmlIcon} alt="" />
+                                <img className="welcome-lista-elemento-linguagens-img" src={linguagemAtual.img} alt="" />
+                                <span className="welcome-lista-elemento-linguagens-text">{linguagemAtual.texto}</span>
                             </div>
-                            <button className="welcome-lista-elemento-avancar ri-arrow-right-s-line"></button>
+                            <button className="welcome-lista-elemento-button ri-arrow-right-s-line" onClick={linguagemAvancar}></button>
                         </div>
                     </div>
                     <div className="welcome-continuar">
                         <button className="welcome-continuar-button">Continuar</button>
                     </div>
                     <div className="welcome-creditos">
-                        <span className="welcome-creditos-text">Desenvolvedor</span>
+                        <span className="welcome-creditos-text">Desenvolvedor: </span>
                         <a className="welcome-creditos-link" href="#">Braion Fernandes</a>
                     </div>
                 </div>
