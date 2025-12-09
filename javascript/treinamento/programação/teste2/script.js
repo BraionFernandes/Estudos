8dc4f9f2ae7844d8ffe2ed819644c494a270a850
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2025-12-09 15:18:58 -0300

    Estudos - Javascript - treinamento - programação
    Desafio Diário concluído.

---
+++ javascript/treinamento/programação/teste2/script.js
@@ -0,0 +1,16 @@
+// Desafio 1: Crie um programa que recebe dois números e exibe o maior deles.
+
+const num1=10;
+const num2=20;
+
+if(num1 > num2){
+    console.log(num1);
+}else{
+    console.log(num2);
+}
+
+// Desafio 2: Crie um contador de 1 a 10 usando for.
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
