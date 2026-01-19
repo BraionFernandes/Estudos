8edee5e2885017a49e99344e2795a0e5aada9f3c
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-01-19 19:59:29 -0300

    React - Treinamento - ClassesB - teste2
    Refazer o Teste 2 sem ajuda;

---
+++ react/treinamento/classesB/teste2/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TodoApp from './assets/components/todoApp'
+
+function App() {
+
+  return (
+    <>
+      <TodoApp/>
+    </>
+  )
+}
+
+export default App
