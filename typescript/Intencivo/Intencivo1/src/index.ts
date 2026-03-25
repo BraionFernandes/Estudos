76cd7764937d7929b593a48f2fa202a46b65088a
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-03-24 21:47:16 -0300

    Estudos - Typescript - Intencivos
    Estudos Intencivos;

---
+++ typescript/Intencivo/Intencivo1/src/index.ts
@@ -0,0 +1,35 @@
+let age: number = 5;
+const name: string = 'Braion';
+const isValid: boolean = true;
+let idk: any = 5;
+
+idk = '5';
+
+const ids: number[] = [1,2,3,4,5];
+const booleans: boolean[] = [true,false,true,false];
+const names: string[] = ['Braion','Tailon','Taisler','Rafaela'];
+
+// Tuplas
+const person: [string,number] = ['Olá, Mundo!', 10];
+
+// Tuplas com listas
+const peaple: [string,number][] = [
+    ['Braion', 22],
+    ['Tailon', 28],
+    ['Taisler', 23]
+];
+
+// Intersections
+const productId: string | number | boolean = false;
+
+
+// Enum
+
+enum Direction{
+    up = 1,
+    down = 2
+}
+
+const direction = Direction.up;
+
+console.log(age);
