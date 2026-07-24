dbcd8a731e15117b63237a05261c1fc430171197
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-07-23 21:55:41 -0300

    Udemy - JS e TS
    Aula 66 - Completed

--- Udemy/javascript-typescript/section3/aula66/script.js
+++ Udemy/javascript-typescript/section3/aula66/script.js
@@ -3,13 +3,9 @@ const btnPausar=document.getElementById('btnPausar');
 const btnZerar=document.getElementById('btnZerar');
 const timer=document.getElementById('timer');
 
-let tempoEmSegundos=55000;
-TimerId=null;
-
-const Contador=setInterval(()=>{
-    tempoEmSegundos++;
-    timer.textContent = formatarTempo(tempoEmSegundos);
-}, 1000);
+let tempoEmSegundos=0;
+let TimerId=null;
+let paused=false;
 
 function formatarTempo(segundos){
     const horas = Math.floor((segundos / 60) / 60);
@@ -23,17 +19,34 @@ function formatarTempo(segundos){
     return `${horaFormatado}:${minFormatado}:${segFormatado}`;
 }
 
-
-
 function inciarTimer(){
     if(TimerId !== null) return;
 
-    TimerId = Contador;
+    TimerId=setInterval(()=>{
+        tempoEmSegundos++;
+        timer.textContent = formatarTempo(tempoEmSegundos);
+    }, 1000);
 }
 
 function pausarTimer(){
-    clearInterval(Contador);
+    if(paused){
+        inciarTimer();
+        paused=false;
+    }else{
+        clearInterval(TimerId);
+        TimerId=null;
+        paused=true;
+    }
+}
+
+function zerarTimer(){
+    clearInterval(TimerId);
+    TimerId=null;
+    tempoEmSegundos=0;
+    timer.textContent='00:00:00';
+    inciarTimer();
 }
 
 btnIniciar.addEventListener('click', inciarTimer);
 btnPausar.addEventListener('click', pausarTimer);
+btnZerar.addEventListener('click', zerarTimer);
