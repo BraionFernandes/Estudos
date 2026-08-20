211047014821836d9584119690921522e59355cc
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-08-20 06:54:27 -0300

    Udemy - Typescript - Aula13

---
+++ typescript/Udemy/Matheus-Fraga/Aula13/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let coisa = 10;
+let coisa2 = "Olá, Mundo!";
+let coisa3 = 5;
+//# sourceMappingURL=index.js.map
---
+++ typescript/Udemy/Matheus-Fraga/Aula13/src/index.ts
@@ -0,0 +1,5 @@
+let coisa:Number=10;
+
+let coisa2:String="Olá, Mundo!";
+
+let coisa3:String|Number=5;
