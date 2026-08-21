62c105f2008c7fb1b880ac3063924ed120eac7f5
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-08-21 06:45:15 -0300

    Index5 - Concluido

--- typescript/braion/estudos/Index5/build/index.js
+++ typescript/braion/estudos/Index5/build/index.js
@@ -1,61 +1,59 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Banco {
-    usuario;
+class Usuario {
+    name;
+    idade;
     cpf;
-    constructor(usuario, cpf) {
-        this.usuario = usuario;
+    id;
+    status;
+    divida;
+    constructor(name, idade, cpf) {
+        this.name = name;
+        this.idade = idade;
         this.cpf = cpf;
+        this.id = Date.now();
+        this.status = true;
+        this.divida = 0;
     }
-}
-class Conta extends Banco {
-    saldo = 0;
-    limiteDeposito = 0;
-    limiteRetirada = 0;
-    constructor(usuario, cpf) {
-        super(usuario, cpf);
-    }
-    PedirLimiteDeposito(limite) {
-        this.limiteDeposito = limite;
-        console.log(`Seu limite de Deposito de: R$ ${limite} foi aprovado, limite atual: R$ ${this.limiteDeposito}`);
-        console.log('---------------------');
-    }
-    PedirLimiteRetirada(limite) {
-        this.limiteRetirada = limite;
-        console.log(`Seu limite de Retirada de: R$ ${limite} foi aprovado, limite atual: R$ ${this.limiteRetirada}`);
-        console.log('---------------------');
-    }
-    Deposito(deposito) {
-        if (deposito <= this.limiteDeposito) {
-            this.saldo += deposito;
-            console.log(`Deposito de: R$ ${deposito} concluido com sucesso! Saldo atual: R$ ${this.saldo}`);
-            console.log('---------------------');
+    AnalisarDivida() {
+        if (this.divida > 0) {
+            console.log(`
+                ------------------
+                User: ${this.name}
+                CPF: ${this.cpf}
+                Status: ${this.status ? "Active" : "Disabled"}
+                ------------------
+                Console: foi encontrado dividas no sistema no valor de: R$ ${this.divida};
+                ------------------
+            `);
         }
         else {
-            console.log(`Não foi possivel efetuar o seu deposito, seu limite de Deposito e de: R$ ${this.limiteDeposito}`);
-            console.log('---------------------');
+            console.log(`
+            ------------------
+            User: ${this.name}
+            CPF: ${this.cpf}
+            Status: ${this.status ? "Active" : "Disabled"}
+            ------------------
+            Console: Não foi encontrado nenhuma irregularidade no sistema;
+            ------------------
+            `);
         }
     }
-    Retirada(retirada) {
-        if (retirada <= this.limiteRetirada) {
-            this.saldo -= retirada;
-            console.log(`Retirada de: R$ ${retirada} concluida com sucesso! Saldo atual: R$ ${this.saldo}`);
-            console.log('---------------------');
-        }
-        else {
-            console.log(`Não foi possivel efetuar sua retirada, seu limite de Retirada e de: R$ ${this.limiteRetirada}`);
-            console.log('---------------------');
-        }
+    AumentarDivida(valor) {
+        this.divida += valor;
+        console.log(`
+            -----------------
+            User: ${this.name}
+            CPF: ${this.cpf}
+            Status: ${this.status ? "Active" : "Disabled"}
+            -----------------
+            Console: Divida do Usuario aumentada para R$ ${this.divida};
+            -----------------
+        `);
     }
 }
-const Braion = new Conta("braion", 18781466102);
-Braion.PedirLimiteDeposito(10000);
-Braion.Deposito(5000);
-Braion.Deposito(5000);
-Braion.Deposito(5000);
-Braion.Deposito(5000);
-Braion.Deposito(5000);
-Braion.Deposito(5000);
-Braion.PedirLimiteDeposito(1000000000);
-Braion.Deposito(100000000);
+const user1 = new Usuario("Braion", 22, "08781466102");
+user1.AnalisarDivida();
+user1.AumentarDivida(10000);
+user1.AnalisarDivida();
 //# sourceMappingURL=index.js.map
---
+++ typescript/braion/estudos/Index5/src/index.ts
@@ -0,0 +1,68 @@
+interface User{
+    name: string;
+    idade: number;
+    cpf: string;
+    id: number;
+    status: boolean;
+}
+
+class Usuario implements User{
+    name: string;
+    idade: number;
+    cpf: string;
+    id: number;
+    status: boolean;
+    divida: number;
+
+    constructor(name:string,idade:number,cpf:string){
+        this.name=name;
+        this.idade=idade;
+        this.cpf=cpf;
+        this.id=Date.now();
+        this.status=true;
+        this.divida=0;
+    }
+
+    AnalisarDivida(){
+        if(this.divida>0){
+            console.log(`
+                ------------------
+                User: ${this.name}
+                CPF: ${this.cpf}
+                Status: ${this.status? "Active":"Disabled"}
+                ------------------
+                Console: foi encontrado dividas no sistema no valor de: R$ ${this.divida};
+                ------------------
+            `);
+        }else{
+            console.log(`
+            ------------------
+            User: ${this.name}
+            CPF: ${this.cpf}
+            Status: ${this.status? "Active":"Disabled"}
+            ------------------
+            Console: Não foi encontrado nenhuma irregularidade no sistema;
+            ------------------
+            `);
+        }
+        
+    }
+    AumentarDivida(valor:number){
+        this.divida+=valor;
+        console.log(`
+            -----------------
+            User: ${this.name}
+            CPF: ${this.cpf}
+            Status: ${this.status? "Active":"Disabled"}
+            -----------------
+            Console: Divida do Usuario aumentada para R$ ${this.divida};
+            -----------------
+        `);
+    }
+}
+
+const user1=new Usuario("Braion",22,"08781466102");
+
+user1.AnalisarDivida();
+user1.AumentarDivida(10000);
+user1.AnalisarDivida();
